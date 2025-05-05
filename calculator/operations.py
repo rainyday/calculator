7317b4ca7c08b9d7d4e9e08a3e8a608514d47108
Alfhi Saputra <muhalfhisaputra@gmail.com>
2025-05-05 14:12:55 +0700

    Create operations.py

---
+++ calculator/operations.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    # TODO: implement this
+    pass
+
+
+def multiply(a, b):
+    # TODO: implement this
+    pass
+
+
+def divide(a, b):
+    # TODO: implement this (don't forget to handle division by zero)
+    pass
